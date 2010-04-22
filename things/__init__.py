9bd0586a0fd8143390a1c99c445d667cd3434bf9
Travis Swicegood <development@domain51.com>
2010-04-22 10:48:32 -0500

    ugly ugly hacks, but it works...

--- things/__init__.py
+++ things/__init__.py
@@ -1,13 +1,19 @@
 import appscript
 
-class Todo(object):
+class AppleScriptHelper(object):
+    """
+    Simple object for wrapping AppleScript objects more sanely.
+
+    Each subclass should provide a ``raw`` property that is a property
+    from ``appscript``.
+    """
     _loaded = False
-    def __init__(self, raw):
-        self.raw = raw
+
+    def __init__(self):
         self._loaded = True
 
     def __getattr__(self, key):
-        if self._loaded:
+        if self._loaded and key != 'set':
             return getattr(self.raw, key).get()
         else:
             return self.__dict__[key]
@@ -18,11 +24,28 @@ class Todo(object):
         else:
             self.__dict__[key] = value
 
-    def __repr__(self):
-        return str(self)
+class TodoAdder(object):
+    def add_to_do(self, **kwargs):
+        properties = dict([(getattr(appscript.k, key), value) for key, value in kwargs.items()])
+        self.raw.make(new=appscript.k.to_do, with_properties=properties)
 
+class NamedObject(object):
+    def __repr__(self):
+        return "<%s: %s>" % (self.__class__.__name__, unicode(self))
+
+    def __unicode__(self):
+        return u"%s" % self.name.encode('ascii', 'ignore')
     def __str__(self):
-        return "%s (%s)" % (self.name, self.status)
+        return self.__unicode__()
+
+class DictLikeObject(object):
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+class Todo(AppleScriptHelper, NamedObject):
+    def __init__(self, raw):
+        self.raw = raw
+        AppleScriptHelper.__init__(self)
 
     def is_complete(self):
         return self.status == appscript.k.completed
@@ -42,16 +65,88 @@ class Todo(object):
     def cancel(self):
         self.status = appscript.k.canceled
 
-class TodoList(object):
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.status)
+
+class TodoList(AppleScriptHelper, NamedObject, TodoAdder):
     def __init__(self, raw):
         self.raw = raw
-        self.to_dos = [Todo(a) for a in self.raw.to_dos.get()]
+        AppleScriptHelper.__init__(self)
 
-class Things(object):
+    @property
+    def to_dos(self):
+        return [Todo(a) for a in self.raw.to_dos.get()]
+
+    def __iter__(self):
+        return iter(self.to_dos)
+
+class ProjectList(AppleScriptHelper, NamedObject, DictLikeObject, TodoAdder):
+    def __init__(self, raw):
+        self.raw = raw
+        for a in self.raw.to_dos.get():
+            self.__dict__[a.name.get()] = TodoList(a)
+        AppleScriptHelper.__init__(self)
+
+    def __getattr__(self, key):
+        try:
+            return super(ProjectList, self).__getattr__(key)
+        except KeyError:
+            return self.__dict__[key]
+
+
+class SpecialList(AppleScriptHelper, DictLikeObject):
+    def __init__(self, raw):
+        self.raw = raw
+        for area in self.raw.get():
+            self.__dict__[area.name.get()] = self.list_of_name(area)
+
+    def __getattr__(self, key):
+        try:
+            return super(AreaList, self).__getattr__(key)
+        except KeyError:
+            return self.__dict__[key]
+
+class Area(TodoList):
+    pass
+class AreaList(SpecialList):
+    list_of_name = Area
+    pass
+
+
+class Person(TodoList):
+    pass
+class PeopleList(SpecialList):
+    list_of_name = Person
+    pass
+
+
+
+class Tag(AppleScriptHelper, NamedObject):
+    def __init__(self, raw):
+        self.raw = raw
+        super(Tag, self).__init__()
+
+class TagList(DictLikeObject):
+    def __init__(self, raw):
+        for tag in raw:
+            self.__dict__[tag.name.get()] = Tag(tag)
+        super(TagList, self).__init__()
+
+    def __repr__(self):
+        return "<%s: %s>" % (self.__class__.__name__, unicode(self.__dict__))
+
+class Things(AppleScriptHelper, TodoAdder):
     def __init__(self):
-        self.app = appscript.app("Things")
-        special = self.app.areas.get() + self.app.people.get()
-        regular = [(a.name.get().lower(), a) for a in self.app.lists.get() if a not in special]
+        self.raw = appscript.app("Things")
+        special = self.raw.areas.get() + self.raw.people.get()
+        regular = [(a.name.get().lower(), a) for a in self.raw.lists.get() if a not in special]
         for key, raw_list in regular:
-            setattr(self, key, TodoList(raw_list))
+            if key == "projects":
+                setattr(self, key, ProjectList(raw_list))
+            else:
+                setattr(self, key, TodoList(raw_list))
+        self.tags = TagList(self.raw.tags.get())
+        self.areas = AreaList(self.raw.areas)
+        self.people = PeopleList(self.raw.people)
+        super(Things, self).__init__()
 
