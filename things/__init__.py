d9f194bbb7e91d6bf56d0070cad814716433c85f
Travis Swicegood <development@domain51.com>
2010-07-24 17:20:28 -0500

    refactor tags a bit to make them more robust

--- things/__init__.py
+++ things/__init__.py
@@ -51,7 +51,30 @@ class DictLikeObject(object):
     def __getitem__(self, key):
         return getattr(self, key)
 
-class Todo(AppleScriptHelper, NamedObject):
+class TaggableItem(object):
+    @property
+    def tags(self):
+        return TagList(self.raw.tags.get())
+
+    def set_tags(self, tags):
+        tags = [tags] if type(tags) is str else tags
+        self.raw.tag_names.set(','.join(tags))
+
+    def add_tag(self, tag):
+        tags = self.tags
+        if tag in tags:
+            return
+        self.tags = tags + tag
+
+    def remove_tag(self, tag):
+        tags = self.tags
+        if tag not in tags:
+            return
+        self.tags = self.tags - tag
+
+
+
+class Todo(AppleScriptHelper, NamedObject, TaggableItem):
     OPEN = appscript.k.open
     COMPLETED = appscript.k.completed
     CANCELED = appscript.k.canceled
@@ -80,26 +103,6 @@ class Todo(AppleScriptHelper, NamedObject):
 
     def move(self, to_list):
         self.raw.move(to=to_list.raw)
-
-    @property
-    def tags(self):
-        return [a.strip() for a in self.raw.tag_names.get().split(',')]
-
-    def set_tags(self, tags):
-        self.raw.tag_names.set(','.join(tags))
-
-    def add_tag(self, tag):
-        tags = self.tags
-        if tag in tags:
-            return
-        self.tags = tags + [tag,]
-
-    def remove_tag(self, tag):
-        tags = self.tags
-        if tag not in tags:
-            return
-        tags.remove(tag)
-        self.tags = tags
 
     def __str__(self):
         return "%s (%s)" % (self.name, self.status)
@@ -156,14 +159,36 @@ class Tag(AppleScriptHelper, NamedObject, TodosProperty):
         self.raw = raw
         super(Tag, self).__init__()
 
+    @property
+    def parent_tag(self):
+        raw = self.raw.parent_tag.get()
+        return None if raw == appscript.k.missing_value else Tag(raw)
+
 class TagList(DictLikeObject):
     def __init__(self, raw):
         for tag in raw:
             self.__dict__[tag.name.get()] = Tag(tag)
         super(TagList, self).__init__()
 
+    def __contains__(self, k):
+        return k in self.__dict__
+
     def __repr__(self):
         return "<%s: %s>" % (self.__class__.__name__, unicode(self.__dict__))
+
+    def __add__(self, k):
+        k = [k, ] if type(k) is str else k
+        return self.values() + k
+
+    def __sub__(self, k):
+        k = [k, ] if type(k) is str else k
+        values = self.values()
+        [values.remove(a) for a in k]
+        return values
+
+    def values(self):
+        return [a.name for a in self.__dict__.values()]
+
 
 class Things(AppleScriptHelper, TodoAdder):
     def __init__(self):
